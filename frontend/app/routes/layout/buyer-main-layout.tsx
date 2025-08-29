4bdd0c4f0f7c9ded6bb9ea4f5032e5d78b566234
s3956092 <s3956092@rmit.edu.vn>
2025-08-30 04:44:19 +0700

    Fix small bug

--- frontend/app/routes/layout/buyer-main-layout.tsx
+++ frontend/app/routes/layout/buyer-main-layout.tsx
@@ -1,7 +1,6 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import Footer from "~/components/footer";
 import NavBar from "~/components/nav-bar";
-import Footer from "~/components/footer";
 
 export default function BuyerMainLayout() {
   const location = useLocation();
